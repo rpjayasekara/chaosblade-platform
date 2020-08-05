6e6b20e118a56a82166b1e5854a3eccf049f9e9b
rpjayasekara <rpjayasekara.16@cse.mrt.ac.lk>
2020-08-05 20:14:15 +0530

    Web app module basic project structure

---
+++ chaosblade-webapp/src/test/java/com/alibaba/chaosblade/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibaba.chaosblade.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
